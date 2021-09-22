3c263c4cdd50b22f26294762de89602f142ec4fc
easy4docker <easy4docker@gmail.com>
2021-09-22 14:00:54 -0700

    Update pdfModule.js

--- app-service/appModules/pdfModule.js
+++ app-service/appModules/pdfModule.js
@@ -1,64 +1,36 @@
 const fs = require('fs'), path = require('path');
 const pdf = require('html-pdf');
 module.exports = class PDF  {
-    constructor(req, res, next) {
-      this.dataFolder = "/var/_shared/PDF";
-      this.startTime = new Date().getTime();
-      this.cfile = '';
-      this.req = req;
-      this.res = res; 
-    }
-    sendPDF() {
-      const me = this;
-      try {
-        const options = { format: 'A4', 
-        'border': {
-          'top': '0.5in',            // default is 0, units: mm, cm, in, px
-          'right': '0.5in',
-          'bottom': '0.5in',
-          'left': '0.5in'
-        }};
+  constructor(req, res, next) {
+    this.dataFolder = "/var/_shared/PDF";
+    this.startTime = new Date().getTime();
+    this.cfile = '';
+    this.req = req;
+    this.res = res; 
+  }
+  sendPDF() {
+    const me = this;
+    try {
+      const options = { format: 'A4', 
+      'border': {
+        'top': '0.5in',            // default is 0, units: mm, cm, in, px
+        'right': '0.5in',
+        'bottom': '0.5in',
+        'left': '0.5in'
+      }};
 
-        pdf.create('doc', options).toFile('fnPDF', (err, res) => {
-          me.res.send('8899');
-         // me.afterProcessFile((!err)? true : false);
-        });
-      } catch (e) {
-        me.res.send('8891');
-      }
-      
-    }
+      pdf.create('html', options, function(err, buffer){
+        me.res.sendFile(buffer.filename);
 
-    processFile() {
-      const me = this;
-      console.log('processFile - ' + me.cfile);
-      const fnPDF = me.dataFolder + '/output/' + me.cfile + '.pdf';
-      fs.readFile(me.dataFolder + '/input/' + me.cfile, 'utf-8', (err, doc)=> {
-        try {
-          const options = { format: 'A4', 
-          'border': {
-            'top': '0.5in',            // default is 0, units: mm, cm, in, px
-            'right': '0.5in',
-            'bottom': '0.5in',
-            'left': '0.5in'
-          }};
-
-          pdf.create(doc, options).toFile(fnPDF, (err, res) => {
-            me.afterProcessFile((!err)? true : false);
-          });
-        } catch (err) {
-         console.log(err.message + '=--->>>' + doc);
-         me.afterProcessFile(false);
-        }
       });
+      /*
+      pdf.create('doc', options).toFile('/tmp/fnPDF.pdf', (err, res) => {
+        me.res.sendFile(res.filename);
+        // me.afterProcessFile((!err)? true : false);
+      });*/
+    } catch (e) {
+      me.res.send('8891');
     }
-    afterProcessFile(success) {
-      const me = this;
-      const targetFolder = (success) ? '/done/' : '/failed/';
-      fs.rename(me.dataFolder + '/input/' + me.cfile, me.dataFolder +  targetFolder + me.cfile, 
-        (err)=> {
-          console.log('Successfully renamed - ' + me.cfile);
-          me.cfile = '';
-        });
-    }
+    
+  }
 }
